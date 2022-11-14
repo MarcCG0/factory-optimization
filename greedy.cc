cefaa972af87926d5e0f0beecf704902967cbcd7
p4tac0 <104589010+p4tac0@users.noreply.github.com>
2022-11-14 19:52:54 +0100

    V6
    Algorisme: ficar un al principi i un al final de la permutació

--- greedy.cc
+++ greedy.cc
@@ -28,7 +28,7 @@ If it contains a 1, it does need it.
 */
 struct Sol {
     int penalty = INT_MAX;
-    vector<int> permutation = {};
+    vector<int> permutation;
     vector<vector<int>> req;
 };
 
@@ -91,6 +91,7 @@ Input read_input(const string& a, Sol& S)
 
     vector<vector<int>> init1(I.M, vector<int>(I.C, -1));
     S.req = init1;
+    S.permutation = vector<int>(I.C, -1);
     return I;
 }
 
@@ -104,117 +105,47 @@ void writeIntoFile(const string& f_o, const vector<int>& s_partial, int a_pen)
         myfile << a << " ";
     myfile << endl;
     myfile.close();
-
-    cout << "Penalty is: " << a_pen << endl;
-    for (int a : s_partial)
-        cout << a << " ";
-    cout << endl;
-}
-
-void prepare_relations(vector<vector<int>>& R, const vector<vector<int>>& upgr)
-{
-
-    // generates symmetric matrix in which (i,j) indicates
-    // how much upgrades classes i and j have in common
-    for (uint k = 0; k < upgr.size(); ++k) {
-        for (uint i = k; i < upgr.size(); ++i) {
-            for (uint j = 0; j < upgr[0].size(); ++j) {
-                if (upgr[k][j] == upgr[i][j] && upgr[k][j] != 0) {
-                    R[k][i] += 1;
-                    if (k != i)
-                        R[i][k] += 1;
-                }
-            }
-        }
-    }
-}
-
-void choose_first_el(vector<int>& permutation, const vector<vector<int>>& R)
-{
-    int best = INT_MIN;
-    int i_b;
-
-    // inspect who requires more upgrades
-    for (uint i = 0; i < R.size(); ++i) {
-        if (best < R[i][i]) {
-            i_b = i;
-            best = R[i][i];
-        }
-    }
-    // add that class to the permutation
-    permutation.push_back(i_b);
-}
-
-int get_similarity(const Input& I, int classe, int class_i, int upgr)
-{
-    // returns wheter classes classe & class_i both need upgrade upgr
-    if (I.upgr[classe][upgr] == I.upgr[class_i][upgr] && I.upgr[classe][upgr] != 0) {
-        return 1;
-    } else {
-        return 0;
-    }
-}
-
-int get_similarities(const Input& I, const vector<vector<int>>& R, const vector<int>& permutation, int classe)
-{
-    int n = permutation.size(); // # elements already placed
-    const auto& [C, M, K, c_e, n_e, prod, upgr] = I;
-
-    int best_actual = 0;
-    int penalty = 0;
-
-    for (int i = 0; i < M; ++i) {
-        int actual_window = n_e[i];
-
-        if (n - actual_window < 0)
-            actual_window = n;
-
-        for (int j = n - actual_window; j < n; ++j) {
-            best_actual += get_similarity(I, classe, permutation[j], i);
-            if (best_actual > c_e[i])
-                penalty += 1;
-        }
-    }
-
-    return penalty;
 }
 
 void generate_perm(Input& I, const vector<vector<int>>& R, vector<int>& permutation, vector<int>& prod, int K)
 {
     int n = I.C;
-
-    choose_first_el(permutation, R);
-
-    --prod[permutation[0]];
-
     // for all cars
-    for (int i = 1; i < n; ++i) {
-        int index;
-        int penalty_min = INT_MAX;
-        int qty_p = -1; // qty left for production
-
+    for (int i = 0; i < n / 2; ++i) {
         // for all possible classes
-        for (int j = 0; j < K; ++j) {
-
-            // if all j-th cars have been produced, skip
-            if (prod[j] == 0)
-                continue;
-
-            int penalty = get_similarities(I, R, permutation, j);
-
-            if (prod[j] > qty_p) {
-                qty_p = prod[j];
-                index = j;
-                penalty_min = penalty;
-            } else if (prod[j] == qty_p && penalty_min > penalty) {
-                qty_p = prod[j];
-                index = j;
-                penalty_min = penalty;
+        int j = -1;
+        int max_cars = -1;
+        for (int u = 0; u < K; ++u) {
+            if (prod[u] > max_cars) {
+                max_cars = prod[u];
+                j = u;
             }
         }
-        // update consequences of having add index to the permutation
-        --prod[index];
-        permutation.push_back(index);
+        // if all j-th cars have been produced, skip
+        if (prod[j] != 0) {
+
+            if (prod[j] >= 2) {
+                permutation[i] = j;
+                permutation[n - i - 1] = j;
+                prod[j] -= 2;
+            }
+            // we know that prod[j] == 1
+            else {
+                // place one and get another with the greater number of pending fabrications
+                permutation[i] = j;
+                --prod[j];
+                int max_prod = -1;
+                int index = 0;
+                for (int k = 0; k < K; ++k) {
+                    if (prod[k] >= max_prod) {
+                        max_prod = prod[k];
+                        index = k;
+                    }
+                }
+                --prod[index];
+                permutation[n - i - 1] = index;
+            }
+        }
     }
 }
 
@@ -274,8 +205,6 @@ void greedy(Sol& S, Input& I, const string& f_o)
     auto& [penalty, permutation, req] = S;
 
     vector<vector<int>> R(K, vector<int>(K, 0));
-
-    prepare_relations(R, upgr);
 
     // greedy algorithm: generate a decent answer
     generate_perm(I, R, permutation, prod, K);
